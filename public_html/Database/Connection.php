a5a59fb2bf521e22c3748a527eb83b3cc167372d
piru72 <parvezdirom2000@gmail.com>
2023-01-29 22:56:52 +0600

    feat (signup) : sign up done

---
+++ public_html/Database/Connection.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "jaj";
+$link = mysqli_connect($host, $user, $password, $db);
+
+// Check connection
+// if (!$link) {
+//     die("Connection failed: " . mysqli_connect_error());
+//   }
+//   echo "Connected successfully";
+?>
