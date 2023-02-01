a6c496c6985fdbf0d1c553f6e44eec7c4246a5ce
piru72 <parvezdirom2000@gmail.com>
2023-02-02 05:34:41 +0600

    fix (homepage) : added table , script

--- public_html/index.php
+++ public_html/index.php
@@ -17,7 +17,7 @@
 
 <body>
 
-<!--Navigation Section-->
+    <!--Navigation Section-->
 
     <nav class="navbar navbar-expand-lg bg-light rounded">
         <div class="container-fluid">
@@ -48,27 +48,75 @@
         </div>
     </nav>
 
-<!--End of Navigation Section-->
+    <!--End of Navigation Section-->
 
-<!--Buttons Section-->
-        <div class="container-fluid">
-            <div class="row" style="margin-top: 20px">
-                <div class="col-8 text-center" style="margin-right:30px;">
-                    <button class="btn btn-outline-secondary btn-lg btn-square m-1">
-                        <i class="fas fa-plus"></i> Add Problems
-                    </button>
-                    <button class="btn btn-outline-secondary btn-lg btn-square m-1">
-                        <i class="fas fa-pen"></i> Solve Problems
-                    </button>
-                    <button class="btn btn-outline-secondary btn-lg btn-square m-1">
-                        <i class="fas fa-trophy"></i> Leaderboard
-                    </button>
+    <!--Buttons Section-->
+    <div class="container-fluid">
+        <div class="row" style="margin-top: 20px">
+            <div class="col-8 text-center" style="margin-right:30px;">
+                <button class="btn btn-outline-secondary btn-lg btn-square m-1">
+                    <i class="fas fa-plus"></i> Add Problems
+                </button>
+                <button class="btn btn-outline-secondary btn-lg btn-square m-1">
+                    <i class="fas fa-pen"></i> Solve Problems
+                </button>
+                <button class="btn btn-outline-secondary btn-lg btn-square m-1">
+                    <i class="fas fa-trophy"></i> Leaderboard
+                </button>
+            </div>
+            <div class="col-3 d-flex justify-content-center" style="background-color: white; height: 60px; border-radius: 10px">
+                <h5 style="margin-top: 20px"><i class="fas fa-bullhorn"></i> Announcements</h5>
+    
+            </div>
+
+           
+            <div class="container-fluid">
+                <div class="col-8" style="margin-top: 20px">
+                    <div class="card bg-light">
+                        <div class="card-body">
+
+                            <div class="p-5 mb-4 bg-light rounded-3">
+                                <div class="container-fluid py-3">
+                                    <h3 class="display-5 fw-bold welcome" style="display: inline-block"></h3>
+                                    <p class="col-md-8 fs-4">Currently Its in Beta phase,if you find any exception or bug please contact
+                                        <a href="mailto:parvezdirom2000@gmail.com">parvezdirom2000@gmail.com</a>
+                                    </p>
+                                    <a class="btn btn-primary btn-lg" href="#" role="button">Join JAJ</a>
+                                    <a class="btn btn-outline-primary btn-lg" href="#" role="button">Host Contest</a>
+                                </div>
+                            </div>
+
+                            <div class="card-footer">
+                                <p class="text-muted">Author: JAJ</p>
+                                <p class="text-muted">Time: Jan 15, 2023</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-        <div class="col-3 d-flex justify-content-center" style="background-color: white; height: 60px; border-radius: 10px">
-            <h5 style="margin-top: 20px"><i class="fas fa-bullhorn"></i> Announcements</h5>
-        </div>
-                <div class="container-fluid">
-                    <div class="col-8" style="margin-top: 20px">
+            </div>
+            <h4>Leaderboard</h4>
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Handle</th>
+                            <th>Solved</th> 
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                   
+                        <tr>
+                            <td>Parvez</td>
+                            <td>100</td>
+                        
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- <div class="container-fluid">
+                <div class="col-8" style="margin-top: 20px">
                     <div class="card bg-light">
                         <div class="card-body">
                             <h2 class="card-title">Inter University Contest</h2>
@@ -80,21 +128,31 @@
                         </div>
                     </div>
                 </div>
-                </div>
-
-            </div>
-</div>
+            </div> -->
 
 
 
 
-<footer>
-    <p>
-        Made with <i class="fa fa-heart"></i> by Freon Devs
-    </p>
-</footer>
-<script src="/resources/library/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js"></script>
+        </div>
+    </div>
+
+
+
+
+    <footer>
+        <p>
+            Made with <i class="fa fa-heart"></i> by Freon Devs
+        </p>
+    </footer>
+    <script src="/resources/library/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.0/typed.min.js" integrity="sha512-zKaK6G2LZC5YZTX0vKmD7xOwd1zrEEMal4zlTf5Ved/A1RrnW+qt8pWDfo7oz+xeChECS/P9dv6EDwwPwelFfQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        var typed = new Typed('.welcome', {
+            strings: ["WELCOME TO FEC OJ(BETA)", "The Place for Competitive Programmers"],
+            typeSpeed: 200
+        });
+    </script>
 </body>
 
 </html>
