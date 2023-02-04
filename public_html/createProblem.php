ec4676143a7feaad263058f1b82db0b79bd9681b
piru72 <parvezdirom2000@gmail.com>
2023-02-04 09:20:51 +0600

    add (header) : header file included for navbar

--- public_html/createProblem.php
+++ public_html/createProblem.php
@@ -20,43 +20,7 @@
 
     <!--Navigation Section-->
 
-    <nav class="navbar navbar-expand-lg bg-light rounded">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#centeredNav" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse d-lg-flex" id="centeredNav">
-                <a class="navbar-brand col-lg-3 me-0" href="#">JAJ</a>
-                <ul class="navbar-nav col-lg-6 justify-content-lg-center">
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='index.php'">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link"  onclick="window.location.href='problemset.php'">Problemset</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='leaderboard.php'">Leaderboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='submissions.php'">Submissions</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" onclick="window.location.href='createProblem.php'">Create</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='ide.php'">IDE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link"  onclick="window.location.href='debugger.php'">Debugger</a>
-                    </li>
-                </ul>
-                <div class="d-lg-flex col-lg-3 justify-content-lg-end">
-                    <button onclick="window.location.href='registration.php'" class="btn btn-primary">Sign In</button>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php include 'header.php'; ?>
 
     <!--End of Navigation Section-->
 
--- public_html/debugger.php
+++ public_html/debugger.php
@@ -32,49 +32,13 @@
 
     <!--Navigation Section-->
 
-    <nav class="navbar navbar-expand-lg bg-light rounded">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#centeredNav" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse d-lg-flex" id="centeredNav">
-                <a class="navbar-brand col-lg-3 me-0" href="#">JAJ</a>
-                <ul class="navbar-nav col-lg-6 justify-content-lg-center">
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='index.php'">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='problemset.php'">Problemset</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='leaderboard.php'">Leaderboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='submissions.php'">Submissions</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='createProblem.php'">Create</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" onclick="window.location.href='ide.php'">IDE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" onclick="window.location.href='debugger.php'">Debugger</a>
-                    </li>
-                </ul>
-                <div class="d-lg-flex col-lg-3 justify-content-lg-end">
-                    <button onclick="window.location.href='registration.php'" class="btn btn-primary">Sign In</button>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php include 'header.php'; ?>
 
     <!--End of Navigation Section-->
     <div class="container-fluid">
         <div class="row main">
             <div class="col-md-8 left-column">
-                
+
                 <div class="problem-details">
                     <form id="submit-form">
 
@@ -89,7 +53,7 @@
                         <h4>Source Code</h4>
                         <textarea name="sourceCode" class="form-control code-editor"></textarea>
 
-                        
+
                     </form>
                 </div>
 
@@ -97,7 +61,7 @@
             <div class="col-md-4 right-column">
                 <form id="submit-form">
 
-                    
+
 
                     <div class="test-cases">
 
