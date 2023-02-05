d0e6b55cfe982b232dfc23b3c715a943117e2e78
piru72 <parvezdirom2000@gmail.com>
2023-02-06 05:25:17 +0600

    add (problem) : submitted for custom output

---
+++ API_TEST/anothertest.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int32_t main()
+{
+    int tc;
+    cin >> tc;
+
+    for (int i = 0; i < tc; i++)
+    {
+        cout << "Congratulations!!" << endl;
+    }
+
+    return 0;
+}
