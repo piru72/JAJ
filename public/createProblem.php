4258f47a83efd6542fa2e353acd51c30fa796ce9
piru72 <parvezdirom2000@gmail.com>
2023-02-06 06:09:22 +0600

    feat (problem) : problem can be created

--- public/createProblem.php
+++ public/createProblem.php
@@ -27,62 +27,71 @@
 
     <div class="container-fluid">
         <div class="row main">
+
             <div class="col-md-12 left-column">
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-            <div class="col-md-6 left-column">
-
-                <div class="form-group">
-
-                    <label>Problem Title(Required)</label>
-                    <textarea class="form-control "></textarea>
-
-                </div>
-                <div class="form-group">
-
-                    <label>Problem Body(Required)</label>
-                    <textarea class="form-control " rows="6"></textarea>
-
-                </div>
 
 
-                <div class="form-group">
-
-                    <label>Note</label>
-                    <textarea class="form-control" rows="6"></textarea>
-
-                </div>
-
-            </div>
-            <div class="col-md-6 right-column">
                 <form id="submit-form">
 
-                    <div class="form-group">
 
-                        <label>Sample Input (Required)</label>
-                        <textarea class="form-control" rows="6"></textarea>
+                    <div class="code-section" style="width:60%; float:left; padding:10px;">
 
+                        <div class="form-group">
+
+                            <label>Problem Title(Required)</label>
+                            <textarea class="form-control " name="problemTitle"></textarea>
+
+                        </div>
+                        <div class="form-group">
+
+                            <label>Problem Body(Required)</label>
+                            <textarea class="form-control " name="problemStatement" rows="6" style="height: 46vh;"></textarea>
+
+                        </div>
+
+
+                        <div class="form-group">
+
+                            <label>Note</label>
+                            <textarea class="form-control" name="note" rows="6"></textarea>
+
+                        </div>
                     </div>
-                    <div class="form-group">
+                    <div class="test-cases" style="width:40%; float:left;">
 
-                        <label>Sample Output(Required)</label>
-                        <textarea class="form-control" rows="6"></textarea>
+                        <div class="form-group">
 
+                            <label>Sample Input (Required)</label>
+                            <textarea class="form-control" name="sampleInput" rows="6"></textarea>
+
+                        </div>
+                        <div class="form-group">
+
+                            <label>Sample Output(Required)</label>
+                            <textarea class="form-control" name="sampleOutput" rows="6"></textarea>
+
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="" class="form-label">Standard Input</label>
+                            <textarea class="form-control" name="standerdInput" id="" rows="6"></textarea>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="" class="form-label">Standard Output</label>
+                            <textarea class="form-control" name="standerdOutput" id="" rows="6" required></textarea>
+                        </div>
+                        <div class="col-md-12 left-column">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
                     </div>
-
-                    <div class="mb-3">
-                        <label for="" class="form-label">Standard Input</label>
-                        <textarea class="form-control" name="standerd_input" id="" rows="6"></textarea>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="" class="form-label">Standard Output</label>
-                        <textarea class="form-control" name="standerd_output" id="" rows="6" required></textarea>
-                    </div>
+                </form>
 
 
             </div>
-            </form>
+
+
+
 
 
         </div>
@@ -94,6 +103,24 @@
 
     </div>
     </div>
+
+
+    <script>
+        const submitForm = document.querySelector('#submit-form');
+        submitForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const formData = new FormData(submitForm);
+            fetch('./Controller/createProblem.php', {
+                    method: 'post',
+                    body: formData
+                })
+                .then(response => response.text())
+                .then(result => {
+                    alert(result);
+
+                });
+        });
+    </script>
 
 
 
