0b6c70f475958d533bce14603cd3029ad60de997
maraj09 <md.marajrashid@gmail.com>
2023-02-09 11:55:12 +0600

    controller hitting successfully

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ define('SITE_NAME', 'JAJ');
 //App Root
 define('APP_ROOT', dirname(dirname(__FILE__)));
 define('URL_ROOT', '/');
-define('URL_SUBFOLDER', '');
+define('URL_SUBFOLDER', 'jaj');
 
 //DB Params
 define('DB_HOST', 'localhost');
