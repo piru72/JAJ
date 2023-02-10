94acf0535a3cd1dbacd8dc466007c6ac5d57d9d4
piru72 <parvezdirom2000@gmail.com>
2023-02-10 23:45:26 +0600

    fix (pages) : moved to right directory

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -12,30 +12,30 @@ class HomeController
   // Homepage action
   public function index(RouteCollection $routes)
   {
-    require_once APP_ROOT . '/public/home.php';
+    require_once APP_ROOT . '/public/view/pages/home.php';
   }
   public function indexProblemset(RouteCollection $routes)
   {
-    require_once APP_ROOT . '/public/problemset.php';
+    require_once APP_ROOT . '/public/view/pages/problemset.php';
   }
   public function indexLeaderboard(RouteCollection $routes)
   {
-    require_once APP_ROOT . '/public/leaderboard.php';
+    require_once APP_ROOT . '/public/view/pages/leaderboard.php';
   }
   public function indexSubmissions(RouteCollection $routes)
   {
-    require_once APP_ROOT . '/public/submissions.php';
+    require_once APP_ROOT . '/public/view/pages/submissions.php';
   }
   public function indexCreate(RouteCollection $routes)
   {
-    require_once APP_ROOT . '/public/createProblem.php';
+    require_once APP_ROOT . '/public/view/pages/createProblem.php';
   }
   public function indexIDE(RouteCollection $routes)
   {
-    require_once APP_ROOT . '/public/ide.php';
+    require_once APP_ROOT . '/public/view/pages/ide.php';
   }
   public function indexDebugger(RouteCollection $routes)
   {
-    require_once APP_ROOT . '/public/debugger.php';
+    require_once APP_ROOT . '/public/view/pages/debugger.php';
   }
 }
