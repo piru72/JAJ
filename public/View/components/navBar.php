c51b563ecf67fc17374beacfd41f486bd0012a01
piru72 <parvezdirom2000@gmail.com>
2023-02-22 04:23:51 +0600

    feat (navbar) : login logout Fixed

--- public/View/components/navBar.php
+++ public/View/components/navBar.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+if (isset($_SESSION['userEmail'])) {
+    // User is already logged in
+    $loggedIn = true;
+    $userEmail = $_SESSION['userEmail'];
+    if (isset($_POST['logout'])) {
+        // Destroy session variable and redirect to home page
+        session_destroy();
+        header("Location: " . $routes->get('home')->getPath());
+        exit;
+    }
+} else {
+    // User is not logged in
+    $loggedIn = false;
+    $userEmail = null;
+}
+?>
+
 <nav class="navbar navbar-expand-lg bg-light rounded">
     <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#centeredNav" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,9 +38,18 @@
                 <li class="nav-item">
                     <a class="nav-link" href="<?php echo $routes->get('submissions')->getPath();  ?>">Submissions</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $routes->get('create')->getPath();  ?>">Create</a>
-                </li>
+                
+                <?php
+
+                if (isset($_SESSION['userEmail'])) {
+                ?>
+            
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo $routes->get('create')->getPath();  ?>">Create</a>
+                    </li>
+                <?php
+                }
+                ?>
                 <li class="nav-item">
                     <a class="nav-link" href="<?php echo $routes->get('ide')->getPath();  ?>">IDE</a>
                 </li>
@@ -30,8 +58,26 @@
                 </li>
             </ul>
             <div class="d-lg-flex col-lg-3 justify-content-lg-end">
-                <a class="btn btn-primary btn-lg" href="<?php echo $routes->get('registration')->getPath();  ?>" role="button">Sign In</a>
+                <?php
+
+                if (!isset($_SESSION['userEmail'])) {
+                ?>
+                    <a class="btn btn-primary btn-lg" href="<?php echo $routes->get('registration')->getPath(); ?>" role="button">Sign In</a>
+                <?php
+                } ?>
+                <?php
+
+                if (isset($_SESSION['userEmail'])) {
+                ?>
+                    <form method="post">
+                        <button class="btn btn-primary btn-lg" name="logout" type="submit">Logout</button>
+                    </form>
+                <?php
+                }
+                ?>
+
             </div>
+
         </div>
     </div>
 </nav>
