2028311b4fa9b2a04b7aec930fe372992e21d540
piru72 <parvezdirom2000@gmail.com>
2023-02-22 05:21:03 +0600

    add (layout) : user profile layout created

--- public/View/pages/userProfile.php
+++ public/View/pages/userProfile.php
@@ -1,27 +1,84 @@
 <?php require_once './View/layout/header.php'; ?>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--Buttons Section-->
 <div class="container-fluid">
-    <div class="col-12" style="margin-top: 20px">
-        <div class="card bg-light">
-            <div class="card-body">
+    <div class="row" style="margin-top: 20px">
+        <div class="col-8 text-center" style="margin-right:30px;">
 
-                <div class="p-5 mb-4 bg-light rounded-3">
-                    <div class="container-fluid py-3">
-
-                        <h1> THIS IS THE USER PROFILE OF </h1>
-
-                        <?php
-                        echo ' <p> '  . $_SESSION['userEmail'] . '</p>'
-                        ?>
-
-
-                        <!-- <a class="btn btn-outline-primary btn-lg" href="#" role="button">Host Contest</a> -->
+            <div class="row col-12 justify-content-center align-items-center g-2">
+                <div class="col text-end">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Contest Arranged</h4>
+                            <p class="card-text">
+                            <h1>23</h1>
+                            </p>
+                        </div>
                     </div>
                 </div>
 
+                <div class="col text-end">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Problem created</h4>
+                            <p class="card-text">
+                            <h1>23</h1>
+                            </p>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col text-end">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Solved</h4>
+                            <p class="card-text">
+                            <h1>24</h1>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+
+        <div class="col-3 d-flex flex-column justify-content-center" style="background-color: white; height: auto; border-radius: 10px; overflow-y: auto;">
+
+            <img src="public/image/DP.png" class="img-fluid rounded" alt="">
+            <h5 style="margin-top: 20px"><i class="fas fa-bullhorn"></i> Announcements</h5>
+            <h6>Email</h6>
+            <h6>insititute</h6>
+            <h6>email</h6>
+            <h6>location</h6>
+            <h6>website</h6>
+            <h6>Last Seen 5 minutes ago</h6>
+            <h6>Joined on Date</h6>
+
+
+
+        </div>
+
+
+
+
+
+
+
+
     </div>
-</div>
-<?php require_once './View/layout/footer.php'; ?>
+
+
+    <?php require_once './View/layout/footer.php'; ?>
