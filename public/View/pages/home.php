1e4c349538ac65e84a7c70e47798b247d2de304a
piru72 <parvezdirom2000@gmail.com>
2023-02-22 11:10:41 +0600

    fix (button) : hidden from home

--- public/View/pages/home.php
+++ public/View/pages/home.php
@@ -6,17 +6,27 @@
 <div class="container-fluid">
   <div class="row" style="margin-top: 20px">
     <div class="col-8 text-center" style="margin-right:30px;">
+      <?php
 
-      <a href="<?php echo $routes->get('create')->getPath();  ?>">
-        <button class="btn btn-outline-secondary btn-lg btn-square m-1">
-          <i class="fas fa-plus"></i> Add Problems
-        </button>
-      </a>
-      <a href="<?php echo $routes->get('problemset')->getPath();  ?>">
-        <button class="btn btn-outline-secondary btn-lg btn-square m-1" onclick="window.location.href='problemset.php'">
-          <i class="fas fa-pen"></i> Solve Problems
-        </button>
-      </a>
+      if (isset($_SESSION['userEmail'])) {
+      ?>
+
+        <a href="<?php echo $routes->get('create')->getPath();  ?>">
+          <button class="btn btn-outline-secondary btn-lg btn-square m-1">
+            <i class="fas fa-plus"></i> Add Problems
+          </button>
+        </a>
+
+
+        <a href="<?php echo $routes->get('problemset')->getPath();  ?>">
+          <button class="btn btn-outline-secondary btn-lg btn-square m-1" onclick="window.location.href='problemset.php'">
+            <i class="fas fa-pen"></i> Solve Problems
+          </button>
+        </a>
+      <?php
+      }
+      ?>
+
       <a href="<?php echo $routes->get('leaderboard')->getPath();  ?>">
         <button class="btn btn-outline-secondary btn-lg btn-square m-1" onclick="window.location.href='leaderboard.php'">
           <i class="fas fa-trophy"></i> Leaderboard
@@ -40,7 +50,7 @@
                 <p class="col-md-8 fs-4">Currently Its in Beta phase,if you find any exception or bug please contact
                   <a href="mailto:parvezdirom2000@gmail.com">parvezdirom2000@gmail.com</a>
                 </p>
-                <a class="btn btn-primary btn-lg" href="<?php echo $routes->get('registration')->getPath();  ?>"role="button">Join JAJ</a>
+                <a class="btn btn-primary btn-lg" href="<?php echo $routes->get('registration')->getPath();  ?>" role="button">Join JAJ</a>
                 <!-- <a class="btn btn-outline-primary btn-lg" href="#" role="button">Host Contest</a> -->
               </div>
             </div>
@@ -54,7 +64,7 @@
       </div>
     </div>
 
-   
+
 
 
 
