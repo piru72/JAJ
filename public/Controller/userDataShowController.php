6c0499813ec7cd23208baea609a747714d52ccc5
piru72 <parvezdirom2000@gmail.com>
2023-02-22 06:49:39 +0600

    feat(count) solve and create count can be shown

--- public/Controller/userDataShowController.php
+++ public/Controller/userDataShowController.php
@@ -12,14 +12,58 @@ $fullName= $row['FullName'];
 
 
 
+// echo '<h6>' . $UserName . '</h6>';
+
+// echo '<h6>'. $email.'</h6>';
+
+function get_problem_created_count($connect)
+{
+
+   
+
+    $email = $_SESSION['userEmail'];
+
+    $sql = "SELECT * FROM users WHERE Email = '$email'";
+    $result = mysqli_query($connect, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    $id = $row['id'];
+
+    $sql = "SELECT COUNT(author) as 'total' FROM problem_sets WHERE author = '$id'";
+    $result = mysqli_query($connect, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $problemCreated = $row['total'];
+    
+    return $problemCreated;
+}
+$problem_created = get_problem_created_count($connect);
+
+function get_problem_solved_count($connect)
+{
+    
+        $email = $_SESSION['userEmail'];
+    
+        $sql = "SELECT * FROM users WHERE Email = '$email'";
+        $result = mysqli_query($connect, $sql);
+        $row = mysqli_fetch_assoc($result);
+    
+        $id = $row['id'];
+    
+        $sql = "SELECT  DISTINCT(problem) COUNT(who) as 'total' FROM submissions WHERE who = '$id' AND verdict = 'Accepted'";
+        $result = mysqli_query($connect, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $problemSolved = $row['total'];
+        
+        return $problemSolved;
+
+}
+$problem_solved = get_problem_created_count($connect);
+
+
 mysqli_close($connect);
-echo '<img src="public/image/DP.png" class="img-fluid rounded" alt="">';
-echo '<h5 style="margin-top: 20px"><i class="fas fa-bullhorn"></i> Details</h5>';
-echo '<h6>' . $fullName. '</h6>';
-echo '<h6>' . $UserName . '</h6>';
-echo '<h6>AUST</h6>';
-echo '<h6>'. $email.'</h6>';
-echo '<h6>Dhaka,Bangladesh</h6>';
-echo '<h6 > <a href="https://github.com/piru72" >piru72 </a></h6>';
-echo '<h6>Last Seen 5 minutes ago</h6>';
-echo '<h6>Joined on 1 Jan 2023</h6>';
+
+
+
+
+
+
--- public/View/pages/userProfile.php
+++ public/View/pages/userProfile.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" href="public/css/style.css">
 </head>
 <?php require_once './View/components/navBar.php'; ?>
+<?php require_once '../public/Controller/userDataShowController.php'; ?>
 
 <!--Buttons Section-->
 <div class="container-fluid">
@@ -12,23 +13,30 @@
         <div class="col-8 text-center" style="margin-right:30px;">
 
             <div class="row col-12 justify-content-center align-items-center g-2">
+
+
+
                 <div class="col text-end">
                     <div class="card">
                         <div class="card-body">
                             <h4 class="card-title">Contest Arranged</h4>
                             <p class="card-text">
-                            <h1>23</h1>
+                            <h1>00</h1>
                             </p>
                         </div>
                     </div>
                 </div>
+
+
 
                 <div class="col text-end">
                     <div class="card">
                         <div class="card-body">
                             <h4 class="card-title">Problem created</h4>
                             <p class="card-text">
-                            <h1>23</h1>
+                                <?php
+                                echo '<h1>' . $problem_created . '</h1>';
+                                ?>
                             </p>
                         </div>
                     </div>
@@ -39,7 +47,9 @@
                         <div class="card-body">
                             <h4 class="card-title">Solved</h4>
                             <p class="card-text">
-                            <h1>24</h1>
+                            <?php
+                                echo '<h1>' . $problem_solved . '</h1>';
+                                ?>
                             </p>
                         </div>
                     </div>
@@ -50,11 +60,21 @@
         <div class="col-3 d-flex flex-column justify-content-center user-card">
 
             <?php require_once '../public/Controller/userDataShowController.php'; ?>
+            <img src="public/image/DP.png" class="img-fluid rounded" alt="">
+            <h5 style="margin-top: 20px"><i class="fas fa-bullhorn"></i> Details</h5>
+            <h6>AUST</h6>
+            <?php
+
+            echo '<h6>' . $UserName . '</h6>';
+            echo '<h6>' . $fullName . '</h6>';
+            echo '<h6>' . $email . '</h6>';
+            ?>
+            <h6>Dhaka,Bangladesh</h6>
+            <h6> <a href="https://github.com/piru72">piru72 </a></h6>
+            <h6>Last Seen 5 minutes ago</h6>
+            <h6>Joined on 1 Jan 2023</h6>
 
         </div>
-
-
-
 
 
 
